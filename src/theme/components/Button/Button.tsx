581bf32cd59ebd4ebce27570a77217f3fefd8d86
Andrii Taranenko <andrii.taranenko@lp-praha.cz>
2023-01-27 19:23:57 +0100

    Some changes

--- src/theme/components/Button/Button.tsx
+++ src/theme/components/Button/Button.tsx
@@ -1,34 +1,10 @@
 import React from "react";
 
 import { TProps } from "./types";
-import { styled } from "~/theme";
+import { StyledButton } from "./styled";
 
 const Button: React.FC<TProps> = ({ children }) => (
   <StyledButton>{children}</StyledButton>
 );
-
-const StyledButton = styled("button")(({ theme: { devices, rem } }) => ({
-  border: `${rem(1)} solid black`,
-  borderRadius: rem(200),
-  fontSize: rem(18),
-  height: rem(50),
-  minWidth: rem(170),
-  paddingInline: rem(5),
-  transitionDuration: "0.2s",
-  transitionProperty: "background-color, color",
-  transitionTimingFunction: "ease-in",
-  willChange: "contents",
-
-  [devices["tablet"]]: {
-    fontSize: rem(16),
-    height: rem(40),
-    maxWidth: rem(130),
-  },
-
-  ":hover": {
-    backgroundColor: "black",
-    color: "white",
-  },
-}));
 
 export { Button };
---
+++ src/theme/components/Button/styled.ts
@@ -0,0 +1,25 @@
+import { styled } from "~/theme";
+
+export const StyledButton = styled("button")(({ theme: { devices, rem } }) => ({
+  border: `${rem(1)} solid black`,
+  borderRadius: rem(200),
+  fontSize: rem(18),
+  height: rem(50),
+  minWidth: rem(170),
+  paddingInline: rem(5),
+  transitionDuration: "0.2s",
+  transitionProperty: "background-color, color",
+  transitionTimingFunction: "ease-in",
+  willChange: "contents",
+
+  [devices["tablet"]]: {
+    fontSize: rem(16),
+    height: rem(40),
+    maxWidth: rem(130),
+  },
+
+  ":hover": {
+    backgroundColor: "black",
+    color: "white",
+  },
+}));
